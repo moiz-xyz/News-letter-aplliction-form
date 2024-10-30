554294ea89294ac1002afcd484f054d5f54f88e3
Your Name <you@example.com>
2024-10-30 15:54:02 +0500

    add

---
+++ main.js
@@ -0,0 +1,8 @@
+function togle(){
+ const news = document.getElementById("newslet");
+ news.classList.toggle("active"); 
+ }
+ function hide(){
+     const moiz = document.getElementById("newslet");
+moiz.classList.toggle("active")    
+ }
